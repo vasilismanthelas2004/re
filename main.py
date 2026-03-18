69045efd03454c0e2703160013623ed5d62b511e
MARIOS-VASILEIOS MANTHELAS <vasilismanthelas2004@gmail.com>
2026-03-19 01:53:56 +0200

    FInd added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 class scraperPage:
@@ -18,19 +19,37 @@ class scraperPage:
             response = requests.get(self.url,headers=headers)
             response.raise_for_status()  # Check if the request was successful
             self.soup = BeautifulSoup(response.text, 'html.parser')
+            return self.soup
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
-    def extractData(self):
-        if self.soup is not None:
-            # Example: Extract all the text from the page
-            text = self.soup.get_text()
-            print(re.findall(r'\b[A-Za-z][a-z]+\b', text)  # Example regex to find capitalized words
-)
-        else:
-            print("Page not loaded. Please call getPage() first.")
+
+
+class Find:
+    def __init__(self, soup):
+        self.soup = soup
+        self.text = self.soup.get_text()
+
+    def extractTokens(self):
+        if self.soup is None:
+            print("Soup object is not initialized.")
+            return []
+        # Use regex to find words (tokens)
+        tokens = re.findall(r'\b[A-Za-z][a-z]+\b', self.text)
+        
+        return tokens
+    
+    def findName(self):
+        if self.soup is None:
+            print("Soup object is not initialized.")
+            return None
+        return re.search(r'[A-Z][a-z]+',self.text).group()
+
+
 
 if __name__ == "__main__":
     url = "https://en.wikipedia.org/wiki/Trikala"  # Replace with the URL you want to scrape
     scraper = scraperPage(url)
-    scraper.getPage()
-    scraper.extractData()
+    page=scraper.getPage()
+    finder=Find(page)
+    name=finder.findName()
+    print(name)
